64f52cd1a5901861c4b81cc77e09a7011f9da315
ACMG <acmg@ACMGui-MacBookPro.local>
2021-09-28 15:20:28 +0900

    modeling

---
+++ task1/learning_model.py
@@ -0,0 +1,118 @@
+import datetime
+
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+
+import tensor_PM25 as t_PM25
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import sklearn.metrics as metrics
+
+data_PM25 = t_PM25.get_one_loc_tensor(37.26,125.57,"20200201-20200229", "TS,U10M,V10M")
+
+X = data_PM25
+Y = data_PM25.iloc[:,0:1]
+
+
+mm = MinMaxScaler()
+ss = StandardScaler()
+
+X_ss = ss.fit_transform(X)
+y_mm = mm.fit_transform(Y)
+
+# Train Data
+X_train = X_ss[:624, :]
+X_test = X_ss[624:, :]
+
+# Test Data
+
+y_train = y_mm[:624, :]
+y_test = y_mm[624:, :]
+
+print("Training Shape", X_train.shape, y_train.shape)
+print("Testing Shape", X_test.shape, y_test.shape)
+
+X_train_tensors = Variable(torch.Tensor(X_train))
+X_test_tensors = Variable(torch.Tensor(X_test))
+y_train_tensors = Variable(torch.Tensor(y_train))
+y_test_tensors = Variable(torch.Tensor(y_test))
+X_train_tensors_final = torch.reshape(X_train_tensors, (X_train_tensors.shape[0], 1, X_train_tensors.shape[1]))
+X_test_tensors_final = torch.reshape(X_test_tensors, (X_test_tensors.shape[0], 1, X_test_tensors.shape[1]))
+print("Training Shape", X_train_tensors_final.shape, y_train_tensors.shape)
+print("Testing Shape", X_test_tensors_final.shape, y_test_tensors.shape)
+
+device = torch.device("cpu") # device
+# print(torch.cuda.get_device_name(0))
+
+class LSTM1(nn.Module):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+        super(LSTM1, self).__init__()
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+        self.input_size = input_size #input size
+        self.hidden_size = hidden_size #hidden state
+        self.seq_length = seq_length #sequence length
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, batch_first=True) #lstm
+        self.fc_1 = nn.Linear(hidden_size, 128) #fully connected 1
+        self.fc = nn.Linear(128, num_classes) #fully connected last layer
+        self.relu = nn.ReLU()
+
+    def forward(self,x):
+        h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(device) #hidden state
+        c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(device) #internal state # Propagate input through LSTM
+        output, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
+        hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
+        out = self.relu(hn)
+        out = self.fc_1(out) #first Dense
+        out = self.relu(out) #relu
+        out = self.fc(out) #Final Output
+        return out
+
+num_epochs = 30000 #1000 epochs
+learning_rate = 0.00001 #0.001 lr
+
+input_size = 4 #number of features
+hidden_size = 4 #number of features in hidden state
+num_layers = 1 #number of stacked lstm layers
+
+num_classes = 1 #number of output classes
+lstm1 = LSTM1(num_classes, input_size, hidden_size, num_layers, X_train_tensors_final.shape[1]).to(device)
+
+loss_function = torch.nn.MSELoss()    # mean-squared error for regression
+optimizer = optim.Adam(lstm1.parameters(), lr=learning_rate)  # adam optimizer
+
+for epoch in range(num_epochs):
+    outputs = lstm1.forward(X_train_tensors_final.to(device)) #forward pass
+    optimizer.zero_grad() #caluclate the gradient, manually setting to 0
+    # obtain the loss function
+    loss = loss_function(outputs, y_train_tensors.to(device))
+    loss.backward() #calculates the loss of the loss function
+    optimizer.step() #improve from loss, i.e backprop
+    if epoch % 100 == 0:
+        print("Epoch: %d, loss: %1.5f" % (epoch, loss.item()))
+
+df_X_ss = ss.transform(data_PM25)
+df_y_mm = mm.transform(data_PM25.iloc[:,0:1])
+
+df_X_ss = Variable(torch.Tensor(df_X_ss)) #converting to Tensors
+df_y_mm = Variable(torch.Tensor(df_y_mm))
+#reshaping the dataset
+df_X_ss = torch.reshape(df_X_ss, (df_X_ss.shape[0], 1, df_X_ss.shape[1]))
+train_predict = lstm1(df_X_ss.to(device)) #forward pass
+data_predict = train_predict.data.detach().cpu().numpy() #numpy conversion
+dataY_plot = df_y_mm.data.numpy()
+
+data_predict = mm.inverse_transform(data_predict)
+dataY_plot = mm.inverse_transform(dataY_plot)
+plt.figure(figsize=(10,6))
+plt.axvline(x=624, c='r', linestyle='--') #size of the training set
+
+plt.plot(dataY_plot, label='Actual Data')
+plt.plot(data_predict, label='Predicted Data')
+plt.title('Time-Series Prediction')
+plt.legend()
+plt.savefig('./prediction.png')
